ceae633ffeb3d17cf1e0b27074ce74206ebdf97c
ayshafathim <127296427+ayshafathim@users.noreply.github.com>
2023-03-10 17:51:33 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,21 @@
+package USTBATCHNO3.JUNIT5TESTCASES;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int n=9;
+    	if(n%2==0)
+    	{
+    System.out.println("The number "+n + " is an even number" );
+    	}
+    	else
+    	{
+    		System.out.println("The number "+n +  " is an odd number" );
+    	}
+    }
+}
