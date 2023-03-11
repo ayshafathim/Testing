0ce434fe34d1267b6943877d112557a208c1a85a
ayshafathim <127296427+ayshafathim@users.noreply.github.com>
2023-03-11 14:40:18 +0530

    Add files via upload

--- test/java/USTBATCHNO3/JUNIT5TESTCASES/VechicleTest.java
+++ test/java/USTBATCHNO3/JUNIT5TESTCASES/VechicleTest.java
@@ -1,5 +1,19 @@
 package USTBATCHNO3.JUNIT5TESTCASES;
 
-public class VechicleTest {
+    import static org.junit.Assert.assertEquals;
 
-}
+    import org.junit.Test;
+
+    public class VechicleTest {
+    	
+    	@Test
+    	public void vehicaltest() {
+    		//String state = "KL";
+    		int dis = 7;
+    		String expected = "Ernakulam";
+    		assertEquals(expected,Vechicle.district(dis));
+    	}
+    }
+
+    
+
